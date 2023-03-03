97ba97c614100d711937b5c3ce6ae14536e230a7
Grigori <petrenkogrigory18@gmail.com>
2023-03-03 21:38:24 +0100

    add functionality

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const http = require('http')
+const app = require('./app')
+
 
 const PORT = process.env.PORT || 5000;
 
-const httpServer = http.createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html')
-    res.end('result')
-});
+const httpServer = http.createServer(app)
+
+
+// const httpServer = http.createServer((req,res)=>{
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html')
+//     res.end('result ')
+// });
 
 httpServer.listen(PORT, ()=>{
     console.log(`Server is listening http://localhost:${PORT}`)
